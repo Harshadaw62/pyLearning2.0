4e9d06f1d8da11e724f4fa00e95df801c5a4ebd8
Harshadaw62 <harshadaw62@gmail.com>
2024-02-03 23:16:44 +0530

    3_feb

---
+++ 3_3_feb_2024/average.py
@@ -0,0 +1,6 @@
+no1 = int(input("Enter first number: "))
+no2 = int(input("Enter second number: "))
+no3 = int(input("Enter third number: "))
+total = no1 + no2 + no3
+avg = float(total / 3)
+print("Total is: ", total,"And Average is: ", avg)
